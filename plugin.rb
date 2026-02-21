11210fb504f6e4669e3e750c685c51d29f8e1025
_clay_ <109293573+TryhardClay@users.noreply.github.com>
2026-02-21 07:18:36 -0600

    Create plugin.rb

---
+++ plugin.rb
@@ -0,0 +1,7 @@
+# name: discourse-mtg-pdhforum
+# about: Renders MTG mana symbols ({W}, {U}, etc.) and card tooltips ([[Card Name]]) for PDH Forum
+# version: 1.0.0
+# authors: PDH Forum
+# url: https://pdhforum.com
+
+register_asset "stylesheets/mtg-pdhforum.scss"
